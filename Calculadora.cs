3afdeab6efc05e8061c2b46edbc8057df9262653
Ariston <aristoncandido.nw@gmail.com>
2023-05-29 16:43:51 -0300

    Voltei pro C#

---
+++ Calculadora.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Bom dia Qual o seu nome ?");
+
+String nome = Console.ReadLine();
+
+Console.WriteLine("Seu nome é " + nome);
+
+Console.WriteLine("");
+
+Console.WriteLine("Gostaria de calcular algumas coisas ?");
+
+Console.WriteLine("Digite Sim para confirmar | Não para cancelar");
+String r = Console.ReadLine();
+
+if(r=="Sim"){
+    Console.WriteLine("Lets Go");
+
+}else{
+
+    Console.WriteLine("Poxa que pena");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
